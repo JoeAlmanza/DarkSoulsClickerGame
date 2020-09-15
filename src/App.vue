4d7bec21cfe85e7a3aaacb56ff949076ce88b377
Joe Almanza <joealmanza2113@gmail.com>
2020-09-14 19:33:37 -0600

    added upgrade

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
   <div id="app" class="container-fluid">
     <div class="row text-center">
       <div class="col-12">
-      <h1 class="text-center text-warning my-3"><u>Soul Harvester</u></h1>
+      <h1 class="text-center text-info my-3"><u>Soul Clicker</u></h1>
       <p class="text-danger">Click Hellkite dragon to harvest souls</p>
       <img alt="Vue logo" class="shadow" src="https://vignette.wikia.nocookie.net/darksouls/images/a/a6/Hellkite_Wyvern_Render.png/revision/latest?cb=20130714154316" height="250px" @click="mine" />
-      <h3 class="text-warning">Souls: {{Math.floor(souls)}}</h3>
-      <button class="mx-1 my-1" v-for="item in upgrades" :key="item.name" @click="buy(item)"><img :src="item.imgUrl"/> <b><u>{{item.name}}</u></b>  <p>{{item.description}}</p>  <b><p class="text-success">${{Math.floor(item.cost)}}</p></b> (Quantity: {{item.quantity}})</button>
+      <h3 class="text-info">Souls: <span class="text-danger"> {{Math.floor(souls)}}</span></h3>
+      <button class="mx-1 my-1" v-for="item in upgrades" :key="item.name" @click="buy(item)"><img class="btnImg" :src="item.imgUrl"/> <b><u>{{item.name}}</u></b> <p>{{item.description}}</p> <b class="text-success"> ${{Math.floor(item.cost)}}</b> (Quantity: {{item.quantity}})</button>
       </div>
     </div>
   </div>
@@ -27,10 +27,10 @@
           dagger: {
             imgUrl: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/Wpn_Dagger.png",
             name: "Dagger",
-            cost: 20,
+            cost: 10,
             multiplier: 1,
             quantity: 0,
-            description: "Adds +1 soul per click with each Dagger owned",
+            description: "Adds +1 soul per click for each Dagger owned",
             click: true
           },
           claymore: {
@@ -39,7 +39,7 @@
             cost: 100,
             multiplier: 5,
             quantity: 0,
-            description: "Adds +5 souls per click with each Claymore owned",
+            description: "Adds +5 souls per click for each Claymore owned",
             click: true
           },
           greatLordGreatsword: {
@@ -48,26 +48,35 @@
             cost: 10000,
             multiplier: 25,
             quantity: 0,
-            description: "Adds +25 souls per click with each GLG owned",
+            description: "Adds +25 souls per click for each GLG owned",
             click: true
           },
           
           fireball: {
             imgUrl: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/Fireball.png",
             name: "Fireball",
-            cost: 50,
+            cost: 30,
             multiplier: 1,
             quantity: 0,
-            description: "Adds +1 soul every 3 seconds with each Fireball owned",
+            description: "Adds +1 soul every 3 seconds for each Fireball owned",
+            click: false
+          },
+          poisonMist: {
+            imgUrl: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/Poison_Mist.png",
+            name: "Poison Mist",
+            cost: 500,
+            multiplier: 5,
+            quantity: 0,
+            description: "Adds +5 souls every 3 seconds for each Poison Mist owned",
             click: false
           },
           greatChaosFireball: {
             imgUrl: "https://darksouls.wiki.fextralife.com/file/Dark-Souls/Great_Chaos_Fireball.png",
             name: "Great Chaos Fireball",
-            cost: 500,
-            multiplier: 5,
+            cost: 1000,
+            multiplier: 10,
             quantity: 0,
-            description: "Adds +5 souls every 3 seconds with each GCF owned",
+            description: "Adds +10 souls every 3 seconds for each GCF owned",
             click: false
           }
         },
@@ -142,6 +151,10 @@
     cursor: pointer;
     border-radius: 5px;
     box-shadow: 0 2px #999;
+  }
+  
+  .btnImg{
+    max-height: 10vh;
   }
 
 </style>
